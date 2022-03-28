b23a1f1571e00c1cc6923e069f93e48c2862ba69
SlickFromMars <minecraftsummer2017@gmail.com>
2022-03-28 12:13:12 -0400

    typeable

---
+++ Able/frame/Structure.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import pyttsx3
+
+print("Loading your AI personal assistant " + myname)
+frame.Structure.speak("Loading your AI personal assistant " + myname)
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
---
+++ Able/main.py
@@ -0,0 +1,52 @@
+import webbrowser
+import time
+import datetime
+from frame.Remember import yourname
+
+# import frame.Structure
+
+
+def speakprint(text):
+    print(text)
+    # frame.Structure.speak(text)
+
+
+def wishme():
+    hour = datetime.datetime.now().hour
+    if hour < 12:
+        speakprint("Good morning, " + yourname)
+    elif hour < 18:
+        speakprint("Good afternoon, " + yourname)
+    else:
+        speakprint("Good evening, " + yourname)
+
+
+myname = 'ABLE'
+
+wishme()
+
+if __name__ == '__main__':
+
+    while True:
+        # speakprint("Tell me how can I help you now?")
+        statement = input("Enter your command: ").lower()  # frame.Structure.takeCommand()
+        statement = statement.lower()
+        if statement == 0:
+            continue
+
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speakprint(myname + ' is shutting down, good bye.')
+            break
+
+        elif 'search' in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(5)
+
+        elif statement == 'rickroll':
+            webbrowser.open_new_tab('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+        elif 'who are you' in statement or 'what can you do' in statement:
+            speakprint('I am ' + myname + ', an AI created by master Slick.')
+            speakprint('ABLE stands for Artifical Buddy Lacking Entity')
+            speakprint('Get it? Because you are my only buddy?')
