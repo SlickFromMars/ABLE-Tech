78a1d76a1e3866f721b78fb47908aa54f3e0fd75
SlickFromMars <minecraftsummer2017@gmail.com>
2022-03-28 12:20:34 -0400

    Update Structure.py

--- Able/frame/Structure.py
+++ Able/frame/Structure.py
@@ -1,29 +1,33 @@
 import speech_recognition as sr
 import pyttsx3
+
+import main
 from main import myname
 
-print("Loading your AI personal assistant " + myname)
-speak("Loading your AI personal assistant " + myname)
+main.speakprint("Loading your AI personal assistant " + myname)
 
-engine=pyttsx3.init('sapi5')
-voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
+
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        audio=r.listen(source)
+        audio = r.listen(source)
 
         try:
-            statement=r.recognize_google(audio,language='en-in')
+            statement = r.recognize_google(audio, language='en-in')
             print(f"user said:{statement}\n")
 
         except Exception as e:
             speak("Pardon me, please say that again")
             return "None"
         return statement
+    
